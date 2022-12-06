d94aec07531a105f3129eeb8d11178953c47168e
bannzai <kingkong999yhirose@gmail.com>
2022-12-06 21:44:46 +0900

    Add test case

--- Tests/AsyncTests/AsyncTests.swift
+++ Tests/AsyncTests/AsyncTests.swift
@@ -2,10 +2,21 @@ import XCTest
 @testable import Async
 
 final class AsyncTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(Async().text, "Hello, World!")
+    func testExample() async throws {
+        @MainActor func run() async -> Int {
+            do {
+                try await Task.sleep(nanoseconds: 1_000_000_000)
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
+            return 1
+        }
+
+        let async = _Async<Int>()
+        async(run)
+        try await Task.sleep(nanoseconds: 2_000_000_000)
+        XCTAssertEqual(async.value, 1)
+        XCTAssertNil(async.error)
+        XCTAssertFalse(async.isLoading)
     }
 }
