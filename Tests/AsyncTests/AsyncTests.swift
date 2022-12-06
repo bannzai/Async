165407508e970cec7994ac7c0202c632d3fca2cc
bannzai <kingkong999yhirose@gmail.com>
2022-12-06 21:55:12 +0900

    Fix test case

--- Tests/AsyncTests/AsyncTests.swift
+++ Tests/AsyncTests/AsyncTests.swift
@@ -44,14 +44,60 @@ final class AsyncTests: XCTestCase {
         }
 
         do { // loading
-            @MainActor func run() async throws -> Int {
-                try await Task.sleep(nanoseconds: 1_000_000_000)
-                fatalError()
+            let async = _Async<Int>()
+
+            XCTAssertEqual(async.value, nil)
+            XCTAssertNil(async.error)
+            XCTAssertTrue(async.isLoading)
+        }
+    }
+
+    func testTask() async throws {
+        do { // success
+            func task() -> Task<Int, Error> {
+                .init { @MainActor in
+                    do {
+                        try await Task.sleep(nanoseconds: 1_000_000_000)
+                    } catch {
+                        XCTFail(error.localizedDescription)
+                    }
+                    return 1
+                }
             }
 
             let async = _Async<Int>()
-            async(run)
+            async(task())
+            try await Task.sleep(nanoseconds: 2_000_000_000)
 
+            XCTAssertEqual(async.value, 1)
+            XCTAssertNil(async.error)
+            XCTAssertFalse(async.isLoading)
+        }
+
+        do { // failure
+            func task() -> Task<Int, Error> {
+                .init { @MainActor in
+                    do {
+                        try await Task.sleep(nanoseconds: 1_000_000_000)
+                    } catch {
+                        XCTFail(error.localizedDescription)
+                    }
+                    throw TestError()
+                }
+            }
+
+            let async = _Async<Int>()
+            async(task())
+            try await Task.sleep(nanoseconds: 2_000_000_000)
+
+            XCTAssertEqual(async.value, nil)
+            XCTAssertNotNil(async.error)
+            XCTAssertFalse(async.isLoading)
+        }
+
+        do { // loading
+            let async = _Async<Int>()
+            
             XCTAssertEqual(async.value, nil)
             XCTAssertNil(async.error)
             XCTAssertTrue(async.isLoading)
