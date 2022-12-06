6fc78d99235b0e674d5f22f34915ce6866dc31ed
bannzai <kingkong999yhirose@gmail.com>
2022-12-06 21:49:22 +0900

    Add test case

--- Tests/AsyncTests/AsyncTests.swift
+++ Tests/AsyncTests/AsyncTests.swift
@@ -1,23 +1,60 @@
 import XCTest
 @testable import Async
 
+private struct TestError: Error { }
+
 final class AsyncTests: XCTestCase {
-    func testExample() async throws {
-        @MainActor func run() async -> Int {
-            do {
-                try await Task.sleep(nanoseconds: 1_000_000_000)
-            } catch {
-                XCTFail(error.localizedDescription)
+    func testAsyncAction() async throws {
+        do { // success
+            @MainActor func run() async -> Int {
+                do {
+                    try await Task.sleep(nanoseconds: 1_000_000_000)
+                } catch {
+                    XCTFail(error.localizedDescription)
+                }
+                return 1
             }
-            return 1
+
+            let async = _Async<Int>()
+            async(run)
+            try await Task.sleep(nanoseconds: 2_000_000_000)
+
+            XCTAssertEqual(async.value, 1)
+            XCTAssertNil(async.error)
+            XCTAssertFalse(async.isLoading)
         }
 
-        let async = _Async<Int>()
-        async(run)
-        try await Task.sleep(nanoseconds: 2_000_000_000)
+        do { // failure
+            @MainActor func run() async throws -> Int {
+                do {
+                    try await Task.sleep(nanoseconds: 1_000_000_000)
+                } catch {
+                    XCTFail(error.localizedDescription)
+                }
+                throw TestError()
+            }
 
-        XCTAssertEqual(async.value, 1)
-        XCTAssertNil(async.error)
-        XCTAssertFalse(async.isLoading)
+            let async = _Async<Int>()
+            async(run)
+            try await Task.sleep(nanoseconds: 2_000_000_000)
+
+            XCTAssertEqual(async.value, nil)
+            XCTAssertNotNil(async.error)
+            XCTAssertFalse(async.isLoading)
+        }
+
+        do { // loading
+            @MainActor func run() async throws -> Int {
+                try await Task.sleep(nanoseconds: 1_000_000_000)
+                fatalError()
+            }
+
+            let async = _Async<Int>()
+            async(run)
+
+            XCTAssertEqual(async.value, nil)
+            XCTAssertNil(async.error)
+            XCTAssertTrue(async.isLoading)
+        }
     }
 }
