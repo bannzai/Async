3175e196d460502f94b85b095a050ec2c4f5cfac
bannzai <kingkong999yhirose@gmail.com>
2023-01-25 13:49:05 +0900

    Add Error Constraints

--- Sources/Async/AsyncView.swift
+++ Sources/Async/AsyncView.swift
@@ -16,33 +16,33 @@ import SwiftUI
 /// }
 /// ```
 ///
-public struct AsyncView<T, S: View, F: View, L: View>: View {
-  public typealias When = (success: (T) -> S, failure: (Error) -> F, loading: () -> L)
+public struct AsyncView<T, E: Error, S: View, F: View, L: View>: View {
+  public typealias When = (success: (T) -> S, failure: (E) -> F, loading: () -> L)
 
-  let action: _Async<T>.Action
+  let action: _Async<T, E>.Action
   let when: When
 
-  public init(_ task: Task<T, Error>, when: When) {
+  public init(_ task: Task<T, E>, when: When) {
     self.action = .task(task)
     self.when = when
   }
 
-  public init(_ action: @escaping () async throws -> T, when: When) {
-    self.action = .action(action)
+  public init(_ action: @escaping @Sendable () async throws -> T, when: When) where E == Error {
+    self.action = .task(.init(operation: action))
     self.when = when
   }
 
-  public init(_ stream: AsyncStream<T>, when: When) {
+  public init(_ stream: AsyncStream<T>, when: When) where E == Never {
     self.action = .stream(stream)
     self.when = when
   }
 
-  public init(_ throwingStream: AsyncThrowingStream<T, Error>, when: When) {
+  public init(_ throwingStream: AsyncThrowingStream<T, E>, when: When) {
     self.action = .throwingStream(throwingStream)
     self.when = when
   }
 
-  @Async<T> var async
+  @Async<T, E> var async
 
   public var body: some View {
     switch async(action).state {
