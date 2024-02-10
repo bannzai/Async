6cb8974bd4e3efeaf39e79c555423527f70ebd52
bannzai <kingkong999yhirose@gmail.com>
2024-02-10 11:54:59 +0900

    Fix indent

--- Example/AsyncExample/StreamPage.swift
+++ Example/AsyncExample/StreamPage.swift
@@ -2,112 +2,112 @@ import Async
 import SwiftUI
 
 struct StreamPage: View {
-    var body: some View {
-        List {
-            Section("Use Async Property") {
-                UseAsyncPropertyWrapper()
-            }
-            Section("Use AsyncView") {
-                UseAsyncView()
-            }
-        }
-        .listStyle(.grouped)
+  var body: some View {
+    List {
+      Section("Use Async Property") {
+        UseAsyncPropertyWrapper()
+      }
+      Section("Use AsyncView") {
+        UseAsyncView()
+      }
     }
+    .listStyle(.grouped)
+  }
 }
 
 private struct UseAsyncPropertyWrapper: View {
-    @Async<String, Never> var async
-    @Async<String, Error> var asyncWithError
+  @Async<String, Never> var async
+  @Async<String, Error> var asyncWithError
 
-    var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Run basically")
+  var body: some View {
+    VStack(alignment: .leading, spacing: 4) {
+      Text("Run basically")
 
-            switch async(Self.stream()).state {
-            case .success(let value):
-                Text(value)
-            case .loading:
-                ProgressView()
-                    .progressViewStyle(.circular)
-            }
-        }
-
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Run with error")
-
-            switch asyncWithError(Self.throwingStream()).state {
-            case .success(let value):
-                Text(value)
-            case .failure(let error):
-                Text(error.errorMessage)
-            case .loading:
-                ProgressView()
-                    .progressViewStyle(.circular)
-            }
-        }
-        .alert(isPresented: .constant(asyncWithError.error != nil), error: asyncWithError.error?.toAlertError()) {
-            Button("Reload") {
-                asyncWithError.resetState()
-            }
-        }
+      switch async(Self.stream()).state {
+      case .success(let value):
+        Text(value)
+      case .loading:
+        ProgressView()
+          .progressViewStyle(.circular)
+      }
     }
 
-    private static func stream() -> AsyncStream<String> {
-        .init { continuation in
-            continuation.yield("Done")
-        }
-    }
+    VStack(alignment: .leading, spacing: 4) {
+      Text("Run with error")
 
-    private static var i = 0
-    private static func throwingStream() -> AsyncThrowingStream<String, Error> {
-        .init { continuation in
-            if i == 0 {
-                i += 1
-                continuation.finish(throwing: "Error")
-            } else {
-                continuation.yield("Done")
-            }
-        }
+      switch asyncWithError(Self.throwingStream()).state {
+      case .success(let value):
+        Text(value)
+      case .failure(let error):
+        Text(error.errorMessage)
+      case .loading:
+        ProgressView()
+          .progressViewStyle(.circular)
+      }
     }
+    .alert(isPresented: .constant(asyncWithError.error != nil), error: asyncWithError.error?.toAlertError()) {
+      Button("Reload") {
+        asyncWithError.resetState()
+      }
+    }
+  }
+
+  private static func stream() -> AsyncStream<String> {
+    .init { continuation in
+      continuation.yield("Done")
+    }
+  }
+
+  private static var i = 0
+  private static func throwingStream() -> AsyncThrowingStream<String, Error> {
+    .init { continuation in
+      if i == 0 {
+        i += 1
+        continuation.finish(throwing: "Error")
+      } else {
+        continuation.yield("Done")
+      }
+    }
+  }
 }
 
 private struct UseAsyncView: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Run basically")
-            AsyncView(Self.stream(), when: (
-                success: { Text($0) },
-                loading: { ProgressView().progressViewStyle(.circular) }
-            ))
-        }
-
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Run with error")
-
-            AsyncView(Self.throwingStream(), when: (
-                success: { Text($0) },
-                failure: { Text($0.errorMessage) },
-                loading: { ProgressView().progressViewStyle(.circular) }
-            ))
-        }
+  var body: some View {
+    VStack(alignment: .leading, spacing: 4) {
+      Text("Run basically")
+      AsyncView(Self.stream(), when: (
+        success: { Text($0) },
+        loading: { ProgressView().progressViewStyle(.circular) }
+      ))
     }
 
-    private static func stream() -> AsyncStream<String> {
-        .init { continuation in
-            continuation.yield("Done")
-        }
-    }
+    VStack(alignment: .leading, spacing: 4) {
+      Text("Run with error")
 
-    private static var i = 0
-    private static func throwingStream() -> AsyncThrowingStream<String, Error> {
-        .init { continuation in
-            if i == 0 {
-                i += 1
-                continuation.finish(throwing: "Error")
-            } else {
-                continuation.yield("Done")
-            }
-        }
+      AsyncView(Self.throwingStream(), when: (
+        success: { Text($0) },
+        failure: { Text($0.errorMessage) },
+        loading: { ProgressView().progressViewStyle(.circular) }
+      ))
     }
+  }
+
+  private static func stream() -> AsyncStream<String> {
+    .init { continuation in
+      continuation.yield("Done")
+    }
+  }
+
+  private static var i = 0
+  private static func throwingStream() -> AsyncThrowingStream<String, Error> {
+    .init { continuation in
+      if i == 0 {
+        i += 1
+        continuation.finish(throwing: "Error")
+      } else {
+        continuation.yield("Done")
+      }
+    }
+  }
 }
 
